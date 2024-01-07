e456b31b76297e1ebca89a167ed9764825d8bede
Dmitriy Orlov <orlof@mail.ru>
2024-01-07 22:27:12 +0500

    ДЗ к лекции «Хранение состояния на клиенте»

---
+++ client-state/popup/task.js
@@ -0,0 +1,30 @@
+const btnCloseModal = document.querySelector('.modal__close');
+const modal = document.querySelectorAll('.modal');
+
+window.addEventListener('load', () => {
+    modal.forEach((modal) => {
+        modal.classList.add('modal_active');
+    });
+    if (getCookie('modal') === 'closet') {
+        modal.classList.remove('modal_active');
+    }
+})
+
+btnCloseModal.addEventListener('click', () => {
+    modal.forEach((modal) => {
+        modal.classList.remove('modal_active');
+    });
+    setCookie('modal', 'closet');
+})
+
+function setCookie(name, value) {
+    document.cookie = name + '=' + value;
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie} `;
+    let parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop();
+    }
+}
